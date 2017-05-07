e079ec5de4ab1de487b295c9be136b00a19e3c57
cale <cdqwertz@gmail.com>
2017-05-07 16:59:19 +0200

    Update server.js

--- js/server.js
+++ js/server.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+var http = require("http");
+
+var my_server = http.createServer(function (request, res) {
+	res.writeHead(200);
+	res.end("Hello World");
+})
+
+my_server.listen(8080);
+console.log("Server running");
