ca3ec479b2daaab34b3848176e3f26ea5152b81d
Namenlossss <ps.bohner@gmail.com>
2017-05-07 17:10:07 +0200

    a

--- js/server.js
+++ js/server.js
@@ -3,6 +3,7 @@ var http = require("http");
 var my_server = http.createServer(function (request, res) {
 	res.writeHead(200);
 	res.end("Hello World");
+	res.sendFile(__dirname + '/../index.html');
 })
 
 my_server.listen(8080);
