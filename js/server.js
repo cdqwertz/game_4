2053ac028485ffdd13e47effcb0f7eecf4756728
Namenlossss <ps.bohner@gmail.com>
2017-05-07 17:26:06 +0200

    test

--- js/server.js
+++ js/server.js
@@ -1,10 +1,20 @@
 var http = require("http");
+const mapWidth=150;
+const mapHeight=100;
+var grid[mapWidth][mapHeight];
+
+function gameStarted(){
+	
+}
 
 var my_server = http.createServer(function (request, res) {
 	res.writeHead(200);
 	res.end("Hello World");
 	res.sendFile(__dirname + '/../index.html');
-})
-
+	
+});
+function processReq(){
+	
+}
 my_server.listen(8080);
 console.log("Server running");
